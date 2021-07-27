931b20849468ac5f94efd69873193699758aa9b2
JuanDiegoSic <diego23jdsc@gmail.com>
2021-07-27 11:24:57 -0500

    Se le agrego funcion while para realizar un ciclo en caso de error

--- js/practica1.js
+++ js/practica1.js
@@ -1,140 +1,147 @@
 let link = document.getElementById("link");
+departamento = false;
+alert("hola?");
 
-let eleccion = prompt(
-  "Escribe uno de los departamentos de Colombia para mas informacion: \n 1.Amazonas     2.Antioquia \n 3.Arauca     4.Atlantico \n 5.Bogota     6.Bolivar \n 7.Boyaca     8.Caldas \n 9.Caqueta     10.Casanare \n 11.Cauca     12.Cesar \n 13.Choco      14.Cordoba \n 15.Cundinamarca     16.Guainia \n 17.Guaviare     18.Huila \n 19.La Guajira     20.Magdalena \n 21.Meta     22.Nariño \n 23.Norte de Santander \n 24.Putumayo     25.Quindio \n 26.Risaralda      27.San Andres \n  28.Santander      29.Sucre \n 30.Tolima \n 31.Valle del cauca   \n 32.Vaupes      33.Vichada"
-);
-
-eleccion = eleccion.toLowerCase();
-
-if (eleccion == "amazonas"){
-  link.innerText="Amazonas";
-  link.href = "http://www.colombiamania.com/departamentos/amazonas.html";
-}
-else if (eleccion == "antioquia"){
-  link.innerText="Antioquia";
-  link.href = "http://www.colombiamania.com/departamentos/antioquia.html";
-}
-else if (eleccion == "arauca"){
-  link.innerText="Arauca";
-  link.href = "http://www.colombiamania.com/departamentos/arauca.html";
-}
-else if (eleccion == "atlantico"){
-  link.innerText="Atlantico";
-  link.href = "http://www.colombiamania.com/departamentos/atlantico.html";
-}
-else if (eleccion == "bogota"){
-  link.innerText="Bogota";
-  link.href = "http://www.colombiamania.com/departamentos/bogota.html";
-}
-else if (eleccion == "bolivar"){
-  link.innerText="Bolivar";
-  link.href = "http://www.colombiamania.com/departamentos/bolivar.html";
-}
-else if (eleccion == "boyaca"){
-  link.innerText="Boyaca";
-  link.href = "http://www.colombiamania.com/departamentos/boyaca.html";
-}
-else if (eleccion == "caldas"){
-  link.innerText="Caldas";
-  link.href = "http://www.colombiamania.com/departamentos/caldas.html";
-}
-else if (eleccion == "caqueta"){
-  link.innerText="Caqueta";
-  link.href = "http://www.colombiamania.com/departamentos/caqueta.html";
-}
-else if (eleccion == "casanare"){
-  link.innerText="Casanare";
-  link.href = "http://www.colombiamania.com/departamentos/casanare.html";
-}
-else if (eleccion == "cauca"){
-  link.innerText="Cauca";
-  link.href = "http://www.colombiamania.com/departamentos/cauca.html";
-}
-else if (eleccion == "cesar"){
-  link.innerText="Cesar";
-  link.href = "http://www.colombiamania.com/departamentos/cesar.html";
-}
-else if (eleccion == "choco"){
-  link.innerText="Choco";
-  link.href = "http://www.colombiamania.com/departamentos/choco.html";
-}
-else if (eleccion == "cordoba"){
-  link.innerText="Cordoba";
-  link.href = "http://www.colombiamania.com/departamentos/cordoba.html";
-}
-else if (eleccion == "cundinamarca"){
-  link.innerText="Cundinamarca";
-  link.href = "http://www.colombiamania.com/departamentos/cundinamarca.html";
-}
-else if (eleccion == "guainia"){
-  link.innerText="Guainia";
-  link.href = "http://www.colombiamania.com/departamentos/guainia.html";
-}
-else if (eleccion == "guaviare"){
-  link.innerText="Guaviare";
-  link.href = "http://www.colombiamania.com/departamentos/guaviare.html";
-}
-else if (eleccion == "huila"){
-  link.innerText="Huila";
-  link.href = "http://www.colombiamania.com/departamentos/huila.html";
-}
-else if (eleccion == "la guajira"){
-  link.innerText="La Guajira";
-  link.href = "http://www.colombiamania.com/departamentos/laguajira.html";
-}
-else if (eleccion == "magdalena"){
-  link.innerText="Magdalena";
-  link.href = "http://www.colombiamania.com/departamentos/magdalena.html";
-}
-else if (eleccion == "meta"){
-  link.innerText="Meta";
-  link.href = "http://www.colombiamania.com/departamentos/meta.html";
-}
-else if (eleccion == "nariño"){
-  link.innerText="Nariño";
-  link.href = "http://www.colombiamania.com/departamentos/nariño.html";
-}
-else if (eleccion == "norte de santander"){
-  link.innerText="Norte de Santander";
-  link.href = "http://www.colombiamania.com/departamentos/nortedesantander.html";
-}
-else if (eleccion == "putumayo"){
-  link.innerText="Putumayo";
-  link.href = "http://www.colombiamania.com/departamentos/putumayo.html";
-}
-else if (eleccion == "quindio"){
-  link.innerText="Quindio";
-  link.href = "http://www.colombiamania.com/departamentos/quindio.html";
-}
-else if (eleccion == "risaralda"){
-  link.innerText="Risaralda";
-  link.href = "http://www.colombiamania.com/departamentos/risaralda.html";
-}
-else if (eleccion == "san andres"){
-  link.innerText="San Andres";
-  link.href = "http://www.colombiamania.com/departamentos/sanandres.html";
-}
-else if (eleccion == "santander"){
-  link.innerText="Santander";
-  link.href = "http://www.colombiamania.com/departamentos/santander.html";
-}
-else if (eleccion == "sucre"){
-  link.innerText="Sucre";
-  link.href = "http://www.colombiamania.com/departamentos/sucre.html";
-}
-else if (eleccion == "tolima"){
-  link.innerText="Tolima";
-  link.href = "http://www.colombiamania.com/departamentos/tolima.html";
-}
-else if (eleccion == "valle del cauca"){
-  link.innerText="Valle del cauca";
-  link.href = "http://www.colombiamania.com/departamentos/valle.html";
-}
-else if (eleccion == "vaupes"){
-  link.innerText="Vaupes";
-  link.href = "http://www.colombiamania.com/departamentos/vaupes.html";
-}
-else if (eleccion == "vichada"){
-  link.innerText="Vichada";
-  link.href = "http://www.colombiamania.com/departamentos/vichada.html";
+while (departamento == false) {
+  let eleccion = prompt(
+    "Escribe uno de los departamentos de Colombia para mas informacion: \n 1.Amazonas     2.Antioquia \n 3.Arauca     4.Atlantico \n 5.Bogota     6.Bolivar \n 7.Boyaca     8.Caldas \n 9.Caqueta     10.Casanare \n 11.Cauca     12.Cesar \n 13.Choco      14.Cordoba \n 15.Cundinamarca     16.Guainia \n 17.Guaviare     18.Huila \n 19.La Guajira     20.Magdalena \n 21.Meta     22.Nariño \n 23.Norte de Santander \n 24.Putumayo     25.Quindio \n 26.Risaralda      27.San Andres \n  28.Santander      29.Sucre \n 30.Tolima \n 31.Valle del cauca   \n 32.Vaupes      33.Vichada"
+  );
+  eleccion = eleccion.toLowerCase();
+  if (eleccion == "amazonas") {
+    link.innerText = "Amazonas";
+    link.href = "http://www.colombiamania.com/departamentos/amazonas.html";
+    departamento = true;
+  } else if (eleccion == "antioquia") {
+    link.innerText = "Antioquia";
+    link.href = "http://www.colombiamania.com/departamentos/antioquia.html";
+    departamento = true;
+  } else if (eleccion == "arauca") {
+    link.innerText = "Arauca";
+    link.href = "http://www.colombiamania.com/departamentos/arauca.html";
+    departamento = true;
+  } else if (eleccion == "atlantico") {
+    link.innerText = "Atlantico";
+    link.href = "http://www.colombiamania.com/departamentos/atlantico.html";
+    departamento = true;
+  } else if (eleccion == "bogota") {
+    link.innerText = "Bogota";
+    link.href = "http://www.colombiamania.com/departamentos/bogota.html";
+    departamento = true;
+  } else if (eleccion == "bolivar") {
+    link.innerText = "Bolivar";
+    link.href = "http://www.colombiamania.com/departamentos/bolivar.html";
+    departamento = true;
+  } else if (eleccion == "boyaca") {
+    link.innerText = "Boyaca";
+    link.href = "http://www.colombiamania.com/departamentos/boyaca.html";
+    departamento = true;
+  } else if (eleccion == "caldas") {
+    link.innerText = "Caldas";
+    link.href = "http://www.colombiamania.com/departamentos/caldas.html";
+    departamento = true;
+  } else if (eleccion == "caqueta") {
+    link.innerText = "Caqueta";
+    link.href = "http://www.colombiamania.com/departamentos/caqueta.html";
+    departamento = true;
+  } else if (eleccion == "casanare") {
+    link.innerText = "Casanare";
+    link.href = "http://www.colombiamania.com/departamentos/casanare.html";
+    departamento = true;
+  } else if (eleccion == "cauca") {
+    link.innerText = "Cauca";
+    link.href = "http://www.colombiamania.com/departamentos/cauca.html";
+    departamento = true;
+  } else if (eleccion == "cesar") {
+    link.innerText = "Cesar";
+    link.href = "http://www.colombiamania.com/departamentos/cesar.html";
+    departamento = true;
+  } else if (eleccion == "choco") {
+    link.innerText = "Choco";
+    link.href = "http://www.colombiamania.com/departamentos/choco.html";
+    departamento = true;
+  } else if (eleccion == "cordoba") {
+    link.innerText = "Cordoba";
+    link.href = "http://www.colombiamania.com/departamentos/cordoba.html";
+    departamento = true;
+  } else if (eleccion == "cundinamarca") {
+    link.innerText = "Cundinamarca";
+    link.href = "http://www.colombiamania.com/departamentos/cundinamarca.html";
+    departamento = true;
+  } else if (eleccion == "guainia") {
+    link.innerText = "Guainia";
+    link.href = "http://www.colombiamania.com/departamentos/guainia.html";
+    departamento = true;
+  } else if (eleccion == "guaviare") {
+    link.innerText = "Guaviare";
+    link.href = "http://www.colombiamania.com/departamentos/guaviare.html";
+    departamento = true;
+  } else if (eleccion == "huila") {
+    link.innerText = "Huila";
+    link.href = "http://www.colombiamania.com/departamentos/huila.html";
+    departamento = true;
+  } else if (eleccion == "la guajira") {
+    link.innerText = "La Guajira";
+    link.href = "http://www.colombiamania.com/departamentos/laguajira.html";
+    departamento = true;
+  } else if (eleccion == "magdalena") {
+    link.innerText = "Magdalena";
+    link.href = "http://www.colombiamania.com/departamentos/magdalena.html";
+    departamento = true;
+  } else if (eleccion == "meta") {
+    link.innerText = "Meta";
+    link.href = "http://www.colombiamania.com/departamentos/meta.html";
+    departamento = true;
+  } else if (eleccion == "nariño") {
+    link.innerText = "Nariño";
+    link.href = "http://www.colombiamania.com/departamentos/nariño.html";
+    departamento = true;
+  } else if (eleccion == "norte de santander") {
+    link.innerText = "Norte de Santander";
+    link.href =
+      "http://www.colombiamania.com/departamentos/nortedesantander.html";
+      departamento = true;
+  } else if (eleccion == "putumayo") {
+    link.innerText = "Putumayo";
+    link.href = "http://www.colombiamania.com/departamentos/putumayo.html";
+    departamento = true;
+  } else if (eleccion == "quindio") {
+    link.innerText = "Quindio";
+    link.href = "http://www.colombiamania.com/departamentos/quindio.html";
+    departamento = true;
+  } else if (eleccion == "risaralda") {
+    link.innerText = "Risaralda";
+    link.href = "http://www.colombiamania.com/departamentos/risaralda.html";
+    departamento = true;
+  } else if (eleccion == "san andres") {
+    link.innerText = "San Andres";
+    link.href = "http://www.colombiamania.com/departamentos/sanandres.html";
+    departamento = true;
+  } else if (eleccion == "santander") {
+    link.innerText = "Santander";
+    link.href = "http://www.colombiamania.com/departamentos/santander.html";
+    departamento = true;
+  } else if (eleccion == "sucre") {
+    link.innerText = "Sucre";
+    link.href = "http://www.colombiamania.com/departamentos/sucre.html";
+    departamento = true;
+  } else if (eleccion == "tolima") {
+    link.innerText = "Tolima";
+    link.href = "http://www.colombiamania.com/departamentos/tolima.html";
+    departamento = true;
+  } else if (eleccion == "valle del cauca") {
+    link.innerText = "Valle del cauca";
+    link.href = "http://www.colombiamania.com/departamentos/valle.html";
+    departamento = true;
+  } else if (eleccion == "vaupes") {
+    link.innerText = "Vaupes";
+    link.href = "http://www.colombiamania.com/departamentos/vaupes.html";
+    departamento = true;
+  } else if (eleccion == "vichada") {
+    link.innerText = "Vichada";
+    link.href = "http://www.colombiamania.com/departamentos/vichada.html";
+    departamento = true;
+  }
+  else{
+    alert("El de partamento que escribio , no es valido intentelo de nuevo");
+  }
 }
